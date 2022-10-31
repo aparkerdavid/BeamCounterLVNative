b01252da2d9dfb5e8db082342105ce395ba22c5e
Parker Rueve <parkerrueve@gmail.com>
2022-10-31 14:06:13 -0400

    Initial Commit

---
+++ BeamCounterLVNative/BeamCounterLVNativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeamCounterLVNativeApp.swift
+//  BeamCounterLVNative
+//
+//  Created by Parker Rueve on 10/31/22.
+//
+
+import SwiftUI
+
+@main
+struct BeamCounterLVNativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
